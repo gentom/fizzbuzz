862bb2d56a7eefa2266b250f7a828a81a82c8cb3
gentom <gento.m.developer@gmail.com>
2017-08-14 19:23:28 +0900

    commit

--- fizzbuzz.scala
+++ fizzbuzz.scala
@@ -1,31 +1,44 @@
 //FizzBuzz
-
+/*
+for(i <- 1 to 50) {
+  if(i % 15 == 0) {
+    println("FizzBuzz")
+  }else if (i % 3 == 0) {
+    println("Fizz")
+  }else if (i % 5 == 0) {
+    println("Buzz")
+  }else {
+    println(i)
+  }
+}
+*/
 
 object FizzBuzz extends App {
 
-
   1 to 50 foreach { n =>
-
 
     println((n % 3, n % 5) match {
 
-
       case (0, 0) => "FizzBuzz"
-
 
       case (0, _) => "Fizz"
 
-
       case (_, 0) => "Buzz"
-
 
       case _ => n
 
-
     })
-
 
   }
 
-
 }
+
+/*
+def FizzBuzz(i: Int) = (i % 3, i % 5) match {
+  case (0, 0) => "FizzBuzz"
+  case (0, _) => "Fizz"
+  case (_, 0) => "Buzz"
+  case _      => i
+}
+(1 to 50).map(FizzBuzz).foreach(println)
+*/
