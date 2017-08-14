30453082883b8dd71dcbee88cc9ec6db51a32bc7
gentom <gento.m.developer@gmail.com>
2017-08-14 18:47:21 +0900

    initial commit

---
+++ fizzbuzz.scala
@@ -0,0 +1,31 @@
+//FizzBuzz
+
+
+object FizzBuzz extends App {
+
+
+  1 to 50 foreach { n =>
+
+
+    println((n % 3, n % 5) match {
+
+
+      case (0, 0) => "FizzBuzz"
+
+
+      case (0, _) => "Fizz"
+
+
+      case (_, 0) => "Buzz"
+
+
+      case _ => n
+
+
+    })
+
+
+  }
+
+
+}
